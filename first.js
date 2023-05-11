0bc54c356e20e30dab72b591212c8f83ba0fdcbd
Alwin Sajan <alwinsajan225@gmail.com>
2023-05-11 10:16:45 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,17 @@
+// function fadein(){
+//     document.getElementById("img").style.opacity = 0.2;
+    
+// }
+// function fadeout(){
+//     document.getElementById("img").style.opacity = 1;
+// }
+// function increase(){
+//     document.getElementById("img2").style.width = '400px';
+// }
+// function decrease(){
+//     document.getElementById("img2").style.width = '250px';
+// }
+function NewHt(){
+    
+    document.getElementById("rth").innerHTML = "Successful"
+}
